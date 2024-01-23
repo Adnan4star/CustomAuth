971ff66e8057996b50201930a2d2a198f33d4a16
Adnan4star <adnanafzal684@gmail.com>
2024-01-23 19:20:59 +0500

    commit

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app');
+@section('content');
+    
+    
+ 
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-8">
+                <div class="card mt-3 p-3">
+                    <h1>Login</h1>
+
+                    @if (session::has('error'))
+                        <p class="text-danger">{{ session::get('error') }}</p>
+                    @endif
+                    @if (session::has('success'))
+                    <p class="text-success">{{ session::get('success') }}</p>
+                    @endif
+
+                    <form method="POST" action="{{route('login')}}" enctype="multipart/form-data">
+                        @csrf
+                        @method('post')
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control" />
+                            @if($errors->has('email'))
+                                <span class="text-danger">{{$errors->first('email')}}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" />
+                            @if($errors->has('password'))
+                                <span class="text-danger">{{$errors->first('password')}}</span>
+                            @endif
+                        </div>
+                        <div class="row">
+                            <div class="col-8 text-left">
+                                <a href="#" class="btn btn-link">Forgot Password?</a>
+                            </div>
+                            <div class="col-4 text-right">
+                                <input type="submit" class="btn btn-primary" value="Login" />
+                            </div>
+                        </div>
+
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app');
+@section('content');
+    
+    
+ 
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-sm-8">
+                <div class="card mt-3 p-3">
+                    <h1>Register</h1>
+
+                    @if (session::has('error'))
+                        <p class="text-danger">{{ session::get('error') }}</p>
+                    @endif
+
+                    <form method="POST" action="{{route('register')}}" enctype="multipart/form-data">
+                        @csrf
+                        @method('post')
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control" placeholder="name"/>
+                            @if($errors->has('name'))
+                                <span class="text-danger">{{$errors->first('name')}}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" name="email" class="form-control" placeholder="email"/>
+                            @if($errors->has('email'))
+                                <span class="text-danger">{{$errors->first('email')}}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" name="password" class="form-control" placeholder="password"/>
+                            @if($errors->has('password'))
+                                <span class="text-danger">{{$errors->first('password')}}</span>
+                            @endif
+                        </div>
+                        <div class="form-group">
+                            <label>Confirm Password</label>
+                            <input type="password" name="password_confirmation" class="form-control" placeholder="password confirmation"/>
+                            {{-- @if($errors->has('password'))
+                                <span class="text-danger">{{$errors->first('password')}}</span>
+                            @endif --}}
+                        </div>
+                        <div class="row">
+                            <div class="col-8 text-left">
+                                <a href="#" class="btn btn-link">Forgot Password?</a>
+                            </div>
+                            <div class="col-4 text-right">
+                                <input type="submit" class="btn btn-primary" value="Login" />
+                            </div>
+                        </div>
+
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
